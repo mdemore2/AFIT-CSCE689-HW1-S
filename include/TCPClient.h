52a9296c43df0aa2082f183fda47d8c9847ce3e2
Mark <mark.demore2@gmail.com>
2020-01-14 11:58:18 -0800

    o server capable of multiple clients, connection successful

--- include/TCPClient.h
+++ include/TCPClient.h
@@ -3,6 +3,17 @@
 
 #include <string>
 #include "Client.h"
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h>
+#include <arpa/inet.h>
+#include <errno.h>  
+#include <sys/types.h>  
+#include <netinet/in.h>  
+#include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros 
 
 // The amount to read in before we send a packet
 const unsigned int stdin_bufsize = 50;
--- include/TCPServer.h
+++ include/TCPServer.h
@@ -2,6 +2,17 @@
 #define TCPSERVER_H
 
 #include "Server.h"
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h>
+#include <arpa/inet.h>
+#include <errno.h>  
+#include <sys/types.h>  
+#include <netinet/in.h>  
+#include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros 
 
 class TCPServer : public Server 
 {
